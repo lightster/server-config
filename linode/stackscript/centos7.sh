cf1519f4fbf845bc3ceb75ec74d8f0702b9890a9
Matt Light <matt.light@illuminateed.com>
2015-05-01 12:24:48 -0700

    Only create keeper user if not yet created
    We want the script to be re-runnable and re-usable
    in Vagrant

--- linode/stackscript/centos7.sh
+++ linode/stackscript/centos7.sh
@@ -18,8 +18,14 @@ KEEPER_KEYS="$KEEPER_SSHDIR/authorized_keys"
 # redirect stdout and stderr to a log file
 exec >>/var/log/stackscript.log 2>&1
 
-useradd -G wheel $KEEPER_USERNAME
-echo "$KEEPER_USERNAME:$KEEPER_PASSWORD" | chpasswd
+if id -u "$KEEPER_USERNAME" >/dev/null 2>&1 ; then
+    echo -n "User '$KEEPER_USERNAME' already exists... skipping"
+else
+    echo -n "Creating '$KEEPER_USERNAME' user..."
+    useradd -G wheel $KEEPER_USERNAME
+    echo "$KEEPER_USERNAME:$KEEPER_PASSWORD" | chpasswd
+    echo " done"
+fi
 
 mkdir -p $KEEPER_SSHDIR
 chmod 0700 $KEEPER_SSHDIR
